43d20f94ab0d9df09e370cee5d161d11044d434a
Pato2763 <daviespatrick2763@gmail.com>
2025-01-03 15:31:31 +0000

    completed increasing triplets sequence

---
+++ leetCode75/array-string-manipulaton/334-increrasing-triplet-sequence.js
@@ -0,0 +1,21 @@
+var increasingTriplet = function (nums) {
+  let firstNum = Infinity;
+  let secondNum = Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log(firstNum, secondNum);
+    if (nums[i] < firstNum) {
+      firstNum = nums[i];
+    } else if (nums[i] < secondNum) {
+      secondNum = nums[i];
+    } else if (nums[i] > secondNum) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const nums = [2, 1, 5, 0, 4, 6];
+const nums2 = [5, 4, 3, 2, 1];
+const nums3 = [20, 100, 10, 12, 5, 13];
+console.log(increasingTriplet(nums3));
