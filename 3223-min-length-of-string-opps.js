81683a9296dbaac812975cdb4fafaa242742949b
Pato2763 <daviespatrick2763@gmail.com>
2025-01-13 17:31:45 +0000

    completed minimum length of string opperation

---
+++ 3223-min-length-of-string-opps.js
@@ -0,0 +1,20 @@
+const minimumLength = (s) => {
+  const letters = {};
+  for (let i = 0; i < s.length; i++) {
+    if (!letters[s[i]]) {
+      letters[s[i]] = 1;
+    } else {
+      letters[s[i]] += 1;
+    }
+  }
+  console.log(letters);
+  let length = s.length;
+  Object.values(letters).forEach((value) => {
+    length -= 2 * Math.floor((value - 1) / 2);
+    console.log(length);
+  });
+  return length;
+};
+
+const s = "abaacbcbb";
+console.log(minimumLength(s));
