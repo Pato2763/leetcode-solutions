1805dccf46f9cdc922b01597765ff04d3ecf2492
Pato2763 <daviespatrick2763@gmail.com>
2025-01-30 03:13:10 +0000

    completed 151 of the leetcode 75

---
+++ leetCode75/array-string-manipulaton/151-reverse-words-in-a-string.js
@@ -0,0 +1,9 @@
+const reverseWords = (s) => {
+  return s
+    .split(" ")
+    .reverse()
+    .filter((word) => word !== "")
+    .join(" ");
+};
+
+console.log(reverseWords("the   sky is blue"));
